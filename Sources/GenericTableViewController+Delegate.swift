d126a281884d8b1a6c126ebdddefd64f0c7a909c
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-06-24 06:09:57 +1000

    Implement selection handling in GenericTableViewController

--- Sources/GenericTableViewController+Delegate.swift
+++ Sources/GenericTableViewController+Delegate.swift
@@ -1,9 +1,21 @@
-//
-//  GenericTableViewController+Delegate.swift
-//  GenericTableViewController
-//
-//  Created by Giovanni on 24/06/2016.
-//
-//
+import UIKit
 
-import Foundation
+extension GenericTableViewController: UITableViewDelegate {
+
+  func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    guard let configurator = tableViewConfigurator else {
+      preconditionFailure("Attempted to interact with the table view without providing a configurator")
+    }
+
+    let identifier = configurator.identifierForIndex(indexPath.row)
+
+    guard let handler = configurator.cellsConfigurations
+      .filter({ $0.identifier == identifier })
+      .first?
+      .selectionHandler else {
+        return
+    }
+
+    handler(configurator.data[indexPath.row])
+  }
+}

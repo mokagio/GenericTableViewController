35fc38c737eea5b849457a2a3714e4694526e20a
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-06-23 20:19:26 +1000

    Add NSLayoutConstraint convenience init for equal attribute constraint

---
+++ Sources/NSLayoutConstraint+EqualConstraint.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+extension NSLayoutConstraint {
+
+  /// Instantiates an `NSLayoutConstraint` that configures the given `attribute`
+  /// as equal for the two given items.
+  internal convenience init(equalingAttribute attribute: NSLayoutAttribute, ofItem item: UIView, toItem otherItem: UIView) {
+    self.init(
+      item: item,
+      attribute: attribute,
+      relatedBy: .Equal,
+      toItem: otherItem,
+      attribute: attribute,
+      multiplier: 1,
+      constant: 0
+    )
+  }
+}

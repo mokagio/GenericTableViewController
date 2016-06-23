31580893ed24fb4537c58877a4e0d89a06d0e7f2
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-06-24 06:20:17 +1000

    Implement Storyboard-less setup for example app

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -6,6 +6,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    window = UIWindow(frame: UIScreen.mainScreen().bounds)
+
+    let dummy = UIViewController()
+    dummy.view.backgroundColor = UIColor.redColor()
+    let navigationController = UINavigationController(rootViewController: dummy)
+
+    window?.rootViewController = navigationController
+
+    window?.makeKeyAndVisible()
     return true
   }
 }

2663008e7903a6fa1a7dad704666aa7b2f7fec5f
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-06-23 17:06:53 +1000

    Add empty target for GenericTableViewController

---
+++ Sources/GenericTableViewController.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for GenericTableViewController.
+FOUNDATION_EXPORT double GenericTableViewControllerVersionNumber;
+
+//! Project version string for GenericTableViewController.
+FOUNDATION_EXPORT const unsigned char GenericTableViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GenericTableViewController/PublicHeader.h>

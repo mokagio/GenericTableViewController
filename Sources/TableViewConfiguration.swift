b043f319b552958a3ad8033a700c748f28eb5107
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-06-24 18:42:24 +1000

    Add public init for TableViewConfiguration

--- Sources/TableViewConfiguration.swift
+++ Sources/TableViewConfiguration.swift
@@ -7,4 +7,10 @@ public struct TableViewConfiguration<T> {
   public let cellsConfigurations: [CellConfiguration<T>]
 
   public let identifierForIndex: Int -> String
+
+  public init(data: [T], cellsConfigurations: [CellConfiguration<T>], identifierForIndex: Int -> String) {
+    self.data = data
+    self.cellsConfigurations = cellsConfigurations
+    self.identifierForIndex = identifierForIndex
+  }
 }

6c8ff85ce998247a2299bb6771ecd1ed30d5cb3c
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-06-26 19:39:44 +1000

    Use GenericTableViewController in example app

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import GenericTableViewController
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -8,13 +9,93 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     window = UIWindow(frame: UIScreen.mainScreen().bounds)
 
-    let dummy = UIViewController()
-    dummy.view.backgroundColor = UIColor.redColor()
-    let navigationController = UINavigationController(rootViewController: dummy)
+    let rootTableViewController = GenericTableViewController()
+    let navigationController = UINavigationController(rootViewController: rootTableViewController)
+
+    let source = [
+      "Touch me to see Ints": {
+        navigationController.pushViewController(self.numberTableViewController(), animated: true)
+      },
+      "Touch me to see String": {
+        navigationController.pushViewController(self.stringsTableViewController(), animated: true)
+      }
+    ]
+
+    let data: [String] = source.keys.map { $0 }
+    let cellsConfigurations: [CellConfiguration<String>] = [
+      CellConfiguration<String>(
+        identifier: "cell",
+        cellClass: UITableViewCell.self,
+        configurator: { value, cell in
+          cell.textLabel?.text = value
+          return cell
+        },
+        handler: { value in
+          guard let handler = source[value] else {
+            return
+          }
+
+          handler()
+      })
+    ]
+    let tableViewConfigurator = TableViewConfiguration<String>(
+      data: data,
+      cellsConfigurations: cellsConfigurations,
+      identifierForIndex: { _ in return "cell" }
+    )
+    rootTableViewController.tableViewConfigurator = tableViewConfigurator.boxedToAny()
 
     window?.rootViewController = navigationController
 
     window?.makeKeyAndVisible()
     return true
+  }
+
+  private func numberTableViewController() -> GenericTableViewController {
+    let data: [Int] = [1,2,3,4,5]
+    let cellsConfigurations: [CellConfiguration<Int>] = [
+      CellConfiguration<Int>(
+        identifier: "cell",
+        cellClass: UITableViewCell.self,
+        configurator: { value, cell in
+          cell.textLabel?.text = "\(value)"
+          return cell
+        }
+      )
+    ]
+    let tableViewConfigurator = TableViewConfiguration<Int>(
+      data: data,
+      cellsConfigurations: cellsConfigurations,
+      identifierForIndex: { _ in return "cell" }
+    )
+
+    return genericTableViewController(tableViewConfigurator)
+  }
+
+  private func stringsTableViewController() -> GenericTableViewController {
+    let data: [String] = ["Lorem", "ipsum", "doloret", "..."]
+    let cellsConfigurations: [CellConfiguration<String>] = [
+      CellConfiguration<String>(
+        identifier: "cell",
+        cellClass: UITableViewCell.self,
+        configurator: { value, cell in
+          cell.textLabel?.text = value
+          return cell
+        }
+      )
+    ]
+    let tableViewConfigurator = TableViewConfiguration<String>(
+      data: data,
+      cellsConfigurations: cellsConfigurations,
+      identifierForIndex: { _ in return "cell" }
+    )
+
+    return genericTableViewController(tableViewConfigurator)
+  }
+
+  private func genericTableViewController<T>(configuration: TableViewConfiguration<T>) -> GenericTableViewController {
+    let genericTableViewController = GenericTableViewController()
+    genericTableViewController.tableViewConfigurator = configuration.boxedToAny()
+    return genericTableViewController
   }
 }

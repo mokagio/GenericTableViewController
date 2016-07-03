3736a3894489cdde27aff34692848582907e89b5
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-07-04 05:14:48 +1000

    Add test for table view config. init with single row config.

---
+++ Tests/TableViewConfigurationSingleInitSpec.swift
@@ -0,0 +1,45 @@
+import Quick
+import Nimble
+import GenericTableViewController
+
+class TableViewConfigurationSingleInitSpec: QuickSpec {
+  override func spec() {
+
+    describe("TableViewConfiguration") {
+
+      context("when initialized with the single row configuration shorthand") {
+        var sut: TableViewConfiguration<Any>!
+        let rowConfiguration = RowConfiguration<Any>(
+          identifier: "an-identifier",
+          cellClass: UITableViewCell.self,
+          configurator: { _, cell in return cell },
+          handler: .None
+        )
+
+        beforeEach {
+          sut = TableViewConfiguration<Any>(data: [], rowsConfiguration: rowConfiguration)
+        }
+
+        it("has an array of row configurations of size 1") {
+          expect(sut.rowsConfigurations.count) == 1
+        }
+
+        it("has an array of row configuratinos containig the given row configuration") {
+          expect(sut.rowsConfigurations.contains({ current -> Bool in
+            // This might not be the best equality check for RowConfiguration,
+            // but it seems enough in the context of this test
+            return current.identifier == rowConfiguration.identifier
+              && current.cellClass == current.cellClass
+          })).to(beTrue())
+        }
+
+        // swiftlint:disable:next line_length
+        it("has a ___ function that always returns the identifier from the given row configuration") {
+          (0...100).forEach { index in
+            expect(sut.identifierForIndex(index)) == rowConfiguration.identifier
+          }
+        }
+      }
+    }
+  }
+}
